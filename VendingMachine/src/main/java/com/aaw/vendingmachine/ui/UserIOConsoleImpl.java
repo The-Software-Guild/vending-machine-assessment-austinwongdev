37d3fd8e63d05dcb8bfc9c542873d7c633652022
Austin Wong <austinwongdev@gmail.com>
2021-08-03 15:47:09 -0700

    Created backbone for VendingMachine, including packages, classes, exceptions, interfaces, test files, and storage files. Edited pom.xml to enable future testing.

---
+++ VendingMachine/src/main/java/com/aaw/vendingmachine/ui/UserIOConsoleImpl.java
@@ -0,0 +1,16 @@
+/*
+ * @author Austin Wong
+ * email: austinwongdev@gmail.com
+ * date: Aug 3, 2021
+ * purpose: 
+ */
+
+package com.aaw.vendingmachine.ui;
+
+/**
+ *
+ * @author Austin Wong
+ */
+public class UserIOConsoleImpl {
+
+}
